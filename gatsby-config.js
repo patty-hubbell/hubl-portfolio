2f5561f7e14ce8b6e60697cf41d15adff2495409
Patrick Hubbell <hubbell.64@osu.edu>
2020-07-22 12:00:18 -0400

    Include a web app manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,5 +5,18 @@
  */
 
 module.exports = {
-  plugins: [],
+  plugins: [
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Hubl.dev`,
+        short_name: `Hubl`,
+        start_url: `/`,
+        background_color: `##003d5b`,
+        theme_color: `#202020`,
+        display: `standalone`,
+        icon: "./static/favicon.ico",
+      },
+    },
+  ],
 }
